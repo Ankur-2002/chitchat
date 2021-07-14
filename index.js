2a339ac75af8d65ded145ed95a1962cb35a84e5f
Ankur-2002 <ankurchaurasia002@gmail.com>
2021-07-14 12:42:54 +0530

    new dep

--- index.js
+++ index.js
@@ -56,7 +56,11 @@ app.listen(port, ()=>{
 
 if(process.env.NODE_ENV === 'production')
 {
-    app.use(express.static('UI/build'))
+    app.use(express.static(path.join(__dirname,'UI/build')));
+
+    app.get('*',(req,res)=>{
+        res.sendFile(path.join(__dirname, "UI/build", "index.html"));
+    });
 }
  
 app.use('/api/user',userrouter);
